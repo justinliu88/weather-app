26a609a946dc2112863ecc262b97565e9bdf88ff
Haoyang Liu <haoyang.liu@uqconnect.edu.au>
2020-05-04 18:37:47 +1000

    Updates

---
+++ precache-manifest.2320d214bc47d626da6bd8c4adc60171.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6ecb6fb715d5a04aa4a09db3170bf6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5cc4e6a89c519f34bf84",
+    "url": "/static/css/main.8a18e573.chunk.css"
+  },
+  {
+    "revision": "9e85d2bfeae646e6afa6",
+    "url": "/static/js/2.0440c843.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0440c843.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cc4e6a89c519f34bf84",
+    "url": "/static/js/main.f8c8b02c.chunk.js"
+  },
+  {
+    "revision": "7805b6922f8664166ff0",
+    "url": "/static/js/runtime-main.4b73b97f.js"
+  }
+]);
